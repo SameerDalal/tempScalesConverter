effc3833aff5b179d3b09828a5db28ed73dc0b66
SameerDalal <sameerdalal747@gmail.com>
2022-01-21 10:59:07 -0500

    Calculates temperature properly.

--- src/main.java
+++ src/main.java
@@ -7,10 +7,6 @@ public class main {
         m.starterMethod();
 
     }
-
-    final double KELVIN_FACTOR = 273.15;
-    final double RANKINE_FACTOR = 459.67;
-
     public void starterMethod() {
         Scanner scan = new Scanner(System.in);
 
@@ -40,15 +36,33 @@ public class main {
                 coord2 = j;
             }
         }
+        System.out.println(coord1 + "\n" + coord2);
         goTo(coord1, coord2, value);
-
-
     }
 
     public void goTo(int x, int y, double amt) {
-        char[][] addOrMultiplyMatrix = {{}};
-        double[][] conversionRatios = {{1.00,459.67,491.67,1.8},
-                                       {-457.67,1.00,35.6,}};
+        String[][][] addOrMultiplyMatrix = {{{"M","M","M"},        {"A","M","M"},     {"M","A","M"},   {"M","M","M"}},
+                                            {{"S","M","M"},        {"M","M","M"},     {"M","A","M"},   {"S","M","M"}},
+                                            {{"S","M","M"},        {"S","M","M"},     {"M","M","M"},   {"S","M","M"}},
+                                            {{"M","M","M"},        {"S","M","A"},     {"A","M","M"},   {"M","M","M"}}};
+        double[][][] conversionRatios =    {{{1.0,1.0,1.0},        {459.67,1.0,1.0},  {1.8,491.67},    {1.8,1.0,1.0}},
+                                            {{457.67,1.0,1.0},     {1.0,1.0,1.0},     {1.8,32,1.0},    {273,1.0,1.0}},
+                                            {{491.67,0.55,1.0},    {32,.55,1.0},      {1.0,1.0,1.0},   {273,1.0,1.0}},
+                                            {{.55,1.0,1.0},        {32,.55,273.},     {273,1.0,1.0},   {1.0,1.0,1.0}}};
 
+
+        for (int z = 0; z < 3; z++) {
+            if (addOrMultiplyMatrix[y][x][z] == "M") {
+                amt = conversionRatios[y][x][z] * amt;
+            }
+            if (addOrMultiplyMatrix[y][x][z] == "S"){
+                amt = amt - conversionRatios[y][x][z];
+            }
+            if (addOrMultiplyMatrix[y][x][z] == "A"){
+                amt = amt + conversionRatios[y][x][z];
+            }
+        }
+
+        System.out.println(amt);
     }
 }
